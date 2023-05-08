fc324b2b005ae94085d4793f004ad234421f16d7
jaeyeong815 <jae0ee@naver.com>
2023-05-08 16:58:34 +0900

    feat: ExpenseMain 컴포넌트 추가

---
+++ src/components/ExpenseMain.jsx
@@ -0,0 +1,18 @@
+export const ExpenseMain = () => {
+  return (
+    <div>
+      Create ExpenseMain Component
+      {/* Left pane */}
+      <div>
+        {/* Todo: 더치페이 헤더 렌더링 */}
+        {/* Todo: 비용 추가 폼 컴포넌트 렌더링 */}
+        {/* Todo: 정산 결과 컴포넌트 렌더링 */}
+      </div>
+      {/* Right pane */}
+      <div>
+        {/* Todo: 그룹명 헤더 렌더링 */}
+        {/* Todo: 비용 리스트 컴포넌트 렌더링 */}
+      </div>
+    </div>
+  );
+};
