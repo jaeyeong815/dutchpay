1ea8a2484a27f138aa44b171e342ee7a03889b4f
jaeyeong815 <jae0ee@naver.com>
2023-05-08 17:38:44 +0900

    test: CreateGroup 테스트 코드 작성

---
+++ src/components/CreateGroup.spec.jsx
@@ -0,0 +1,43 @@
+import { CreateGroup } from './CreateGroup';
+
+const { render, screen } = require('@testing-library/react');
+const { default: userEvent } = require('@testing-library/user-event');
+
+const renderComponent = () => {
+  render(<CreateGroup />);
+  const input = screen.getByPlaceholderText('2023 제주도 여행');
+  const saveBtn = screen.getByText('저장');
+  const errorMessage = screen.queryByText('그룹 이름을 입력해주세요.');
+
+  return {
+    input,
+    saveBtn,
+    errorMessage,
+  };
+};
+
+describe('그룹 생성 페이지', () => {
+  test('그룹 이름 입력 컴포넌트가 렌더링 되는가', () => {
+    const { input, saveBtn } = renderComponent();
+    // todo: input 컴포넌트 렌더링
+    expect(input).not.toBeNull();
+    // todo: save 버튼 렌더링
+    expect(saveBtn).not.toBeNull();
+  });
+
+  test('그룹명을 입력하지 않고 "저장" 버튼 클릭 시, 에러메시지 노출', async () => {
+    const { saveBtn, errorMessage } = renderComponent();
+
+    await userEvent.click(saveBtn);
+    expect(errorMessage).not.toBeNull();
+  });
+
+  test('그룹명을 입력 후 "저장" 버튼클릭 시, 저장 성공', async () => {
+    const { input, saveBtn, errorMessage } = renderComponent();
+
+    await userEvent.type(input, '예시 그룹명');
+    await userEvent.click(saveBtn);
+
+    expect(errorMessage).toBeNull();
+  });
+});
