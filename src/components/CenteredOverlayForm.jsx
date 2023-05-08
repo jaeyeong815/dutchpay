02432be57e901c8e8bdb12e59e553730addfed22
jaeyeong815 <jae0ee@naver.com>
2023-05-08 23:17:12 +0900

    feat: Overlay 스타일 컴포넌트 추가

--- src/components/CenteredOverlayForm.jsx
+++ src/components/CenteredOverlayForm.jsx
@@ -1,3 +1,28 @@
-export const CenteredOverlayForm = () => {
-  return <div>Create CenteredOverlayForm Component</div>;
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import { OverlayWrapper } from './shared/OverlayWrapper';
+
+export const CenteredOverlayForm = ({ children }) => {
+  return (
+    <StyledCentralizedContainer>
+      <StyledHeader>Dutch Pay</StyledHeader>
+      <OverlayWrapper>{children}</OverlayWrapper>
+    </StyledCentralizedContainer>
+  );
 };
+
+const StyledCentralizedContainer = styled(Container)`
+  width: 50vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  gap: 50px;
+`;
+
+const StyledHeader = styled.h1`
+  font-weight: 200;
+  letter-spacing: 10px;
+`;
---
+++ src/components/shared/OverlayWrapper.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const OverlayWrapper = ({ children }) => <StyledContainer>{children}</StyledContainer>;
+
+const StyledContainer = styled.div`
+  min-height: ${(props) => props.minHeight || '0'};
+  padding: ${(props) => props.padding || '5vw'};
+  background-color: white;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 15px;
+`;
