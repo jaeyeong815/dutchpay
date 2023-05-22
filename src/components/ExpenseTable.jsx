96740ed974af4eeb67828f977995b03e349e4b85
jaeyeong815 <jae0ee@naver.com>
2023-05-22 22:48:25 +0900

    design: ExpenseTable 컴포넌트 스타일링

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,29 +1,48 @@
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
 import { Table } from 'react-bootstrap';
 import { expensesState } from '../state/expenses';
+import { OverlayWrapper } from './shared/OverlayWrapper';
 
 export const ExpenseTable = () => {
   const expenses = useRecoilValue(expensesState);
   return (
-    <Table data-testid='expenseList' borderless hover responsive>
-      <thead>
-        <tr>
-          <th>날짜</th>
-          <th>내용</th>
-          <th>결제자</th>
-          <th>금액</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expenses.map(({ date, desc, amount, payer }) => (
+    <OverlayWrapper minHeight={'73vh'}>
+      <Table data-testid='expenseList' borderless hover responsive>
+        <StyledThead>
           <tr>
-            <td>{date}</td>
-            <td>{desc}</td>
-            <td>{payer}</td>
-            <td>{parseInt(amount)} 원</td>
+            <th>날짜</th>
+            <th>내용</th>
+            <th>결제자</th>
+            <th>금액</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </StyledThead>
+        <StyledTbody>
+          {expenses.map(({ date, desc, amount, payer }, idx) => (
+            <tr key={`expense-${idx}`}>
+              <td>{date}</td>
+              <td>{desc}</td>
+              <td>{payer}</td>
+              <td>{parseInt(amount)} 원</td>
+            </tr>
+          ))}
+        </StyledTbody>
+      </Table>
+    </OverlayWrapper>
   );
 };
+
+const StyledThead = styled.thead`
+  color: #6b3da6;
+  text-align: center;
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+const StyledTbody = styled.tbody`
+  td {
+    text-align: center;
+    font-weight: 400;
+    font-size: 24px;
+  }
+`;
