807e473b0ecd593d2c51cec659607d64a11c4e0f
jaeyeong815 <jae0ee@naver.com>
2023-05-22 22:48:50 +0900

    design: ExpenseMain RigthPane 스타일링

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,21 +1,51 @@
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import { Col, Container, Row } from 'react-bootstrap';
+import { groupNameState } from '../state/groupName';
 import { AddExpenseForm } from './AddExpenseForm';
 import { ExpenseTable } from './ExpenseTable';
 
 export const ExpenseMain = () => {
   return (
-    <div>
-      Create ExpenseMain Component
-      {/* Left pane */}
-      <div>
-        {/* Todo: 더치페이 헤더 렌더링 */}
-        <AddExpenseForm />
-        {/* Todo: 정산 결과 컴포넌트 렌더링 */}
-      </div>
-      {/* Right pane */}
-      <div>
-        {/* Todo: 그룹명 헤더 렌더링 */}
-        <ExpenseTable />
-      </div>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} sm={5} md={4}>
+          <LeftPane />
+        </Col>
+        <Col>
+          <RightPane />
+        </Col>
+      </Row>
+    </Container>
   );
 };
+
+const LeftPane = () => (
+  <Container>
+    {/* Todo: 더치페이 헤더 렌더링 */}
+    <AddExpenseForm />
+    {/* Todo: 정산 결과 컴포넌트 렌더링 */}
+  </Container>
+);
+const RightPane = () => {
+  const groupName = useRecoilValue(groupNameState);
+  return (
+    <StyledContainer>
+      <Row>
+        <StyledGroupName>{groupName || '그룹 이름'}</StyledGroupName>
+      </Row>
+      <ExpenseTable />
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled(Container)`
+  padding: 100px 31px;
+`;
+
+const StyledGroupName = styled.h2`
+  margin-bottom: 80px;
+  text-align: center;
+  font-weight: 700;
+  font-size: 48px;
+`;
