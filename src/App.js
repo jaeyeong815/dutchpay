d96cc64f528172cb12d4b2c20ab191a63102b48c
jaeyeong815 <jae0ee@naver.com>
2023-05-24 12:18:24 +0900

    feat: 라우팅 주소 상수화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
+import { ROUTES } from './routes';
 import { CreateGroup } from './components/CreateGroup';
 import { AddMembers } from './components/AddMembers';
 import { ExpenseMain } from './components/ExpenseMain';
@@ -12,9 +13,10 @@ const App = () => {
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<CreateGroup />} />
-          <Route path='/members' element={<AddMembers />} />
-          <Route path='/expense' element={<ExpenseMain />} />
+          <Route path='/' element={<Navigate to={ROUTES.CREATE_GROUP} />} />
+          <Route path={ROUTES.CREATE_GROUP} element={<CreateGroup />} />
+          <Route path={ROUTES.ADD_MEMBERS} element={<AddMembers />} />
+          <Route path={ROUTES.EXPENSE_MAIN} element={<ExpenseMain />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
